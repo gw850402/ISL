7e702f0a0769bc477828b599d985bbbf99b9f5a6
zhw <362595851@qq.com>
2019-11-19 19:22:48 +0800

    1

---
+++ WebServer/JingJia.Connect/ConnectService/plc/SendCommand.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="SendCommand.ashx.cs" Class="ConnectService.plc.SendCommand" %>
---
+++ WebServer/JingJia.Connect/ConnectService/plc/SendCommand.ashx.cs
@@ -0,0 +1,44 @@
+﻿using Jingjia.PLCModel;
+using JingJia.PLCComm;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectService.plc
+{
+    /// <summary>
+    /// SendCommand 的摘要说明
+    /// </summary>
+    public class SendCommand : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+
+            int code = 0;
+            float result = 0F;
+            string str = "";
+            context.Response.ContentType = "text/plain";
+
+            if (int.TryParse(context.Request.Params[0], out code))
+            {
+                PLCDeviceBase pLCDeviceBase = JingJia.PLCDriver.CommandQueueDriver.ExecuteCommand(code, EnumHandleType.电表抄录);
+                str = JsonConvert.SerializeObject(pLCDeviceBase);
+            }
+            else
+            {
+                code = 1;
+            }
+            context.Response.Write(Common.ResultJsonString(code, str, ""));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
