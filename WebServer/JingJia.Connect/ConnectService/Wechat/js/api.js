31617572e5a1b3f6f57d07216d12bfebdf2ef9f0
高伟 <gaow_1985@163.com>
2019-12-01 12:05:29 +0800

    一体水阀页面调试

--- WebServer/JingJia.Connect/ConnectService/Wechat/js/api.js
+++ WebServer/JingJia.Connect/ConnectService/Wechat/js/api.js
@@ -1,6 +1,6 @@
 
 const BASEURL = "/plc/";
 
-const SendCommand = "../../plc/SendCommand.ashx?";
+const SendCommand = "../../../plc/SendCommand.ashx?";
 
-const WebSocketUrl = "ws://localhost:8080/msg"
+const WebSocketUrl = "ws://localhost:8080/msg";
