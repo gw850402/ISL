d2af61ba31414da16331d308263bfc8e8619a18a
zhw <362595851@qq.com>
2019-12-02 16:51:26 +0800

    COM

--- WebServer/JingJia.Connect/ConnectService/Wechat/js/api.js
+++ WebServer/JingJia.Connect/ConnectService/Wechat/js/api.js
@@ -3,4 +3,6 @@ const BASEURL = "/plc/";
 
 const SendCommand = "../../../plc/SendCommand.ashx?";
 
+const SystemCOM = "../../../plc/COMHandler.ashx?";
+
 const WebSocketUrl = "ws://localhost:8080/msg";
